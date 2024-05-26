bb30e84e926c5f27e81dbacb6567952e975723ef
yewum1 <partsa660@gmail.com>
2024-05-26 23:27:42 +0400

    jjj

--- app.js
+++ app.js
@@ -1,18 +1,95 @@
 const express = require("express");
+const session = require('express-session');
 const app = express();
 const path = require("path"); 
+const ejs = require('ejs');
+const fs = require('fs');
+const bodyParser = require('body-parser');
 
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-const domain = 'http://localhost:3000/'
-
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public'))); 
+app.use(bodyParser.json());
+
+app.use(session({
+    secret: 'tregfdjy',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }   
+}));
 
 
-app.get('/admin', async (req, res) =>{
+function isAuthenticated(req, res, next) {
+    if (req.session.user) {
+        next();
+    } else {
+        res.redirect('/login');
+    }
+
+}
+
+app.get('/login',  async (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
+
+const bcrypt = require('bcryptjs');
+app.post('/login', async (req, res) => {
+
+    const { login, password } = req.body;
+
+    const admin = await prisma.admin.findUnique({
+        where: {login}
+    })
+   if ( admin.password === password) {
+    req.session.user = admin;
+    res.redirect('/admin')
+   }
+});
+
+// Logout route
+app.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) {
+            return res.send('Error logging out');
+        }
+        res.redirect('/login');
+    });
+});
+app.post('/delete', async (req, res) => {
+    const { id } = req.body;
+    try {
+        await prisma.application.delete({
+            where: {
+                id: parseInt(req.body.id)
+            }
+        });
+        res.status(204).send(); 
+    } catch (error) {
+        console.error('Ошибка при удалении заявки:', error);
+        res.status(500).send('Внутренняя ошибка сервера');
+    }
+});
+app.get('/admin', isAuthenticated, async (req, res) =>{
+  
+    const { sortType } = req.query;
+
+    let filter = {};
+    
+    if (sortType) {
+        if (sortType.includes(',')) {
+            // If there are multiple sortTypes separated by commas
+            const sortTypes = sortType.split(',').map(Number);
+            filter.type = { in: sortTypes };
+        } else {
+            // If there's only one sortType
+            filter.type = parseInt(sortType, 10);
+        }
+    }
     try {
         const data = await prisma.application.findMany({
+            where: filter,
             orderBy:{
                 createdAt: 'desc'
             }
@@ -27,9 +104,40 @@ app.get('/admin', async (req, res) =>{
         const renderedHTML = ejs.render(template, { data });
 
         res.send(renderedHTML);
-    } catch {
-
+    } catch (error) {
+        console.log('Error', error)
+        res.status(500).send('Internal server error.')
     }
 })
 
-module.exports = app;
+app.post('/api/send', async (req, res) => {
+    try {
+        const { type, name, email,desc, phone,from,to } = req.body;
+
+        const FormData = await prisma.application.create({
+            data: {
+                type, 
+                name, 
+                email,
+                desc,
+                phone,
+                from,
+                to
+            }
+        })
+        return res.status(200).json({ status: 200, message: "Success" });
+    } catch (e) {
+        console.error("Error:", e);
+        return res
+            .status(500)
+            .send({ status: 500, message: "Internal server error" });
+    }
+})
+
+
+// module.exports = app;
+
+
+app.listen(3000, () => {
+    console.log('server start')
+});
--- public/script.js
+++ public/script.js
@@ -1,4 +1,9 @@
-const form = document.querySelector("#form");
+const form_con = document.querySelector("#form_con");
+const form_modal = document.querySelector('#form_modal')
+
+var type;
+const btns = document.querySelectorAll('.btn-send')
+
 const FORM_2 = document.querySelector('#formId');
 const submitBtn = document.querySelector(".btn-order");
 var btnloader = document.querySelectorAll(".btn-loader");
@@ -6,6 +11,13 @@ const toaster = document.querySelector(".toaster");
 var btn = document.querySelectorAll('.btn');
 
 const toasterErr = document.querySelector('.toasterErr');
+
+
+btns.forEach(button => {
+  button.addEventListener('click', ()=> {
+    type = parseInt(button.getAttribute('data-type'));
+  })
+})
 
 window.addEventListener("load", () => {
   const loader = document.querySelector(".loader");
@@ -18,3 +30,52 @@ window.addEventListener("load", () => {
 });
 
 
+form_con.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData.entries());
+  
+  const type = 1; // Assuming type is a constant here
+  data.type = type; // Adding the type to the data
+  sendformData(data);
+  e.target.reset();
+});
+form_modal.addEventListener("submit", (e) => {
+  e.preventDefault();
+    
+  const formData = new FormData(e.target);
+  const data = Object.fromEntries(formData.entries());
+  
+  data.type = type; 
+
+  sendformData(data);
+  e.target.reset();
+})
+
+function sendformData(data) {
+  fetch('/api/send', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log("formData sent successfully");
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
+
+function zayavka(e) {
+   e.target.reset();
+  setTimeout(hideToaster, 1100);
+  setTimeout(() => sendFeedback(feedback), 1100);
+}
